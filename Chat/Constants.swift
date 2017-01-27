a37f75af48148a6738d5f15baa58f0be3421e58a
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2017-01-27 13:07:06 +0700

    Constants file has been initiated

---
+++ Chat/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  Chat
+//
+//  Created by Zulwiyoza Putra on 1/27/17.
+//  Copyright © 2017 zulwiyozaputra. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    // MARK: NotificationKeys
+    
+    struct NotificationKeys {
+        static let SignedIn = "onSignInCompleted"
+    }
+    
+    // MARK: MessageFields
+    
+    struct MessageFields {
+        static let name = "name"
+        static let text = "text"
+        static let imageUrl = "photoUrl"
+    }
+}
